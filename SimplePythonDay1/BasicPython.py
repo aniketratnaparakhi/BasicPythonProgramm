b88c61a9e82b27ead928a8e7c2728ca1f2ad3498
aniketratnaparakhi <55104024+aniketratnaparakhi@users.noreply.github.com>
2020-07-09 17:21:10 +0530

    Add files via upload

---
+++ SimplePythonDay1/BasicPython.py
@@ -0,0 +1,21 @@
+# edc
+
+# Hellow World
+
+print("Hellow World")
+
+#o/p:- Hellow World
+
+# Arithmatic Operation
+print(2+3) #Addition 5
+print(2*3) #Multiplication 6
+print(5-6) #substraction -1
+print(4/2) #Division 2.0
+print(2**2) #Square 4
+print(2**3) #cube
+
+Num1= int(input("Enter the 1st Number \n"))# take a intiger
+Num2= int(input("Enter the 2nd Number \n"))# take a intiger
+print(Num1+Num2)
+print(Num1*Num2)
+print(Num1-Num2)
